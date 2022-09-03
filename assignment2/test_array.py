ff4116501eb7bfff083d8c19d3b70c6fc07e78a9
leanderf <leanderf@ifi.uio.no>
2022-09-04 00:17:39 +0200

    implement 2d array tests

--- assignment2/test_array.py
+++ assignment2/test_array.py
@@ -131,19 +131,73 @@ def test_mean_1d():
 
 
 def test_add_2d():
-    pass
+    # Add 1 to array of ints
+    array = Array((2,2), 1, 2, 3, 4)
+    array = array + 1
+    assert array == Array((2,2), 2, 3, 4, 5)
+
+    # Add 1 to array of floats
+    array = Array((2,2), 1.1, 2.2, 3.3, 4.4)
+    array = array + 1
+    assert array == Array((2,2), 2.1, 3.2, 4.3, 5.4)
+
+    # Add 1.1 to array of floats
+    array = Array((2,2), 1.0, 2.0, 3.0, 4.0)
+    array = array + 1.1
+    assert array == Array((2,2), 2.1, 3.1, 4.1, 5.1)
+
+    # Add array of floats to 2
+    array = Array((2,2), 1.0, 2.0, 3.0, 4.0)
+    array = 2 + array
+    assert array == Array((2,2), 3.0, 4.0, 5.0, 6.0)
+
+    # Add array with another array
+    array = Array((2,2), 1.0, 2.0, 3.0, 4.0)
+    array = array + Array((2,2), 5, 2, 4, 10)
+    assert array == Array((2,2), 6, 4, 7, 14)
 
 
 def test_mult_2d():
-    pass
+    # Multiply 2 to array of ints
+    array = Array((2,2), 1, 2, 3, 4)
+    array = array * 2
+    assert array == Array((2,2), 2, 4, 6, 8)
+
+    # Multiply 2 to array of floats
+    array = Array((2,1), 2.2, 2.2)
+    array = array * 2
+    assert array == Array((2,1), 4.4, 4.4)
+
+    # Multiply 1.1 to array of floats
+    array = Array((3,1), 1.0, 2.0, 4.0)
+    array = array * 1.1
+    assert array == Array((3,1), 1.1, 2.2, 4.4)
+
+    # Multiply array of floats to 2
+    array = Array((2,2), 1.0, 2.0, 3.0, 4.0)
+    array = 2 * array
+    assert array == Array((2,2), 2.0, 4.0, 6.0, 8.0)
+
+    # Multiply array with another array
+    array = Array((2,2), 1.0, 2.0, 3.0, 4.0)
+    array = array * Array((2,2), 5, 2, 4, 10)
+    assert array == Array((2,2), 5, 4, 12, 40)
+
 
 
 def test_same_2d():
-    pass
+    array = Array((2,2), 1, 2, 3, 4)
+    array2 = Array((2,2), 1, 2, 3, 4)
+    assert array.is_equal(array2) == Array((2,2), 1, 1, 1, 1)
+
+    array2 = Array((2,2), 1, 2, 42, 4)
+    assert array.is_equal(array2) == Array((2,2), 1, 1, 0, 1)
+    assert array.is_equal(array2) == Array((2,2), True, True, False, True)
 
 
 def test_mean_2d():
-    pass
+    array = Array((2,2), 1, 2, 3, 4)
+    assert array.mean_element() == 2.5
 
 
 if __name__ == "__main__":
