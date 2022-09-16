03c8c302fa6c3d8fd5547a6a50af38b54854d2f1
leanderf <leanderf@ifi.uio.no>
2022-09-16 18:29:44 +0200

    python color2gray implementation

--- assignment3/instapy/python_filters.py
+++ assignment3/instapy/python_filters.py
@@ -6,6 +6,8 @@ import numpy as np
 def python_color2gray(image: np.array) -> np.array:
     """Convert rgb pixel array to grayscale
 
+    DOES NOT SUPPORT ALPHA CHANNELS
+
     Args:
         image (np.array)
     Returns:
@@ -14,7 +16,12 @@ def python_color2gray(image: np.array) -> np.array:
     gray_image = np.empty_like(image)
     # iterate through the pixels, and apply the grayscale transform
 
-    ...
+    for row in range(image.shape[0]):  # height
+        for col in range(image.shape[1]):  # width
+            red, green, blue = image[row][col]
+            weighted_sum = red*0.21 + green*0.72 + blue*0.07
+            gray_image[row][col] = [weighted_sum, weighted_sum, weighted_sum]
+
     return gray_image
 
 
@@ -35,3 +42,10 @@ def python_color2sepia(image: np.array) -> np.array:
     # Return image
     # don't forget to make sure it's the right type!
     return sepia_image
+
+
+if __name__ == '__main__':
+    from instapy import io
+    image = io.read_image(filename='assignment3/test_image.jpg')
+    gray_image = python_color2gray(image)
+    io.display(gray_image)
