22220125fffe66f02ff8080fb03eb172f9ebc47c
leanderf <leanderf@ifi.uio.no>
2022-09-17 00:44:19 +0200

    add filter timing report functionality

--- assignment3/instapy/timing.py
+++ assignment3/instapy/timing.py
@@ -5,11 +5,11 @@ Can be executed as `python3 -m instapy.timing`
 
 For Task 6.
 """
-import time
-import instapy
-from . import io
-from typing import Callable
+from __init__ import get_filter
+from instapy.io import read_image
 import numpy as np
+import time
+from typing import Callable
 
 
 def time_one(filter_function: Callable, *arguments, calls: int = 3) -> float:
@@ -30,9 +30,14 @@ def time_one(filter_function: Callable, *arguments, calls: int = 3) -> float:
         time (float):
             The average time (in seconds) to run filter_function(*arguments)
     """
-    # run the filter function `calls` times
-    # return the _average_ time of one call
-    ...
+    timings = []
+    for _ in range(calls):
+        t = time.time_ns()
+        filter_function(*arguments)
+        runtime = (time.time_ns() - t)
+        timings.append(runtime)
+
+    return np.average(timings)
 
 
 def make_reports(filename: str = "test/rain.jpg", calls: int = 3):
@@ -44,31 +49,49 @@ def make_reports(filename: str = "test/rain.jpg", calls: int = 3):
         filename (str): the image file to use
     """
 
+    report_output = ""
+
     # load the image
-    image = ...
+    image = read_image(filename)
+
     # print the image name, width, height
-    ...
+    report_image_data = f"Timing performed using {filename}: {image.shape[1]}x{image.shape[0]}\n"
+    report_output += f"{report_image_data}\n"
+    print(report_image_data)
+
     # iterate through the filters
-    filter_names = ...
+    filter_names = ["color2gray"]
+    # filter_names = ["color2gray", "color2sepia"]
     for filter_name in filter_names:
         # get the reference filter function
-        reference_filter = ...
+        reference_filter = get_filter(filter_name, implementation="python")
+
         # time the reference implementation
-        reference_time = ...
-        print(
-            f"Reference (pure Python) filter time {filter_name}: {reference_time:.3}s ({calls=})"
-        )
+        reference_time = time_one(reference_filter, image, calls=calls)
+
+        report_reference_data = f"Reference (pure Python) filter time {filter_name}: {int(reference_time) / 1_000_000_000:.3}s ({calls=})"
+        report_output += f"{report_reference_data}\n"
+        print(report_reference_data)
+        
         # iterate through the implementations
-        implementations = ...
+        implementations = ["numpy", "numba"]
         for implementation in implementations:
-            filter = ...
-            # time the filter
-            filter_time = ...
+            filter = get_filter(filter_name, implementation=implementation)
+            filter_time = time_one(filter, image, calls=calls)
+            
             # compare the reference time to the optimized time
-            speedup = ...
-            print(
-                f"Timing: {implementation} {filter_name}: {filter_time:.3}s ({speedup=:.2f}x)"
-            )
+            speedup = reference_time / filter_time
+
+            report_implementation_result = f"Timing: {implementation} {filter_name}: {int(filter_time) / 1_000_000_000:.3}s ({speedup=:.2f}x)"
+            report_output += f"{report_implementation_result}\n"
+            print(report_implementation_result)
+
+        report_output += '\n'
+        print('\n')
+
+    # save to file
+    with open("timing-report.txt", 'w', encoding='utf-8') as f:
+        f.write(report_output)
 
 
 if __name__ == "__main__":
