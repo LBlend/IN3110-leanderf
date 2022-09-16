46b8d8380fec6696f6f4a7fcea824b8a8ca983ca
leanderf <leanderf@ifi.uio.no>
2022-09-17 01:29:32 +0200

    color2sepia python implementation

--- assignment3/instapy/python_filters.py
+++ assignment3/instapy/python_filters.py
@@ -34,10 +34,20 @@ def python_color2sepia(image: np.array) -> np.array:
         np.array: sepia_image
     """
     sepia_image = np.empty_like(image)
-    # Iterate through the pixels
-    # applying the sepia matrix
 
-    ...
+    sepia_matrix = [
+        [ 0.393, 0.769, 0.189],
+        [ 0.349, 0.686, 0.168],
+        [ 0.272, 0.534, 0.131],
+    ]
+    
+    for row in range(image.shape[0]):  # height
+        for col in range(image.shape[1]):  # width
+            red, green, blue = image[row][col]
+            avg_weighted_red = min(255, (red * sepia_matrix[0][0] + green * sepia_matrix[0][1] + blue * sepia_matrix[0][2]) / 3)
+            avg_weighted_green = min(255, (red * sepia_matrix[1][0] + green * sepia_matrix[1][1] + blue * sepia_matrix[1][2]) / 3)
+            avg_weighted_blue = min(255, (red * sepia_matrix[2][0] + green * sepia_matrix[2][1] + blue * sepia_matrix[2][2]) / 3)
+            sepia_image[row][col] = [avg_weighted_red, avg_weighted_green, avg_weighted_blue]
 
     # Return image
     # don't forget to make sure it's the right type!
@@ -46,6 +56,9 @@ def python_color2sepia(image: np.array) -> np.array:
 
 if __name__ == '__main__':
     from instapy import io
-    image = io.read_image(filename='assignment3/test_image.jpg')
+    image = io.read_image(filename='assignment3/test_image2.jpg')
     gray_image = python_color2gray(image)
     io.display(gray_image)
+
+    sepia_image = python_color2sepia(image)
+    io.display(sepia_image)
