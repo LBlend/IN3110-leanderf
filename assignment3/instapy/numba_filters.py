cacd172e3c32a5674862830aa85666207822debd
leanderf <leanderf@ifi.uio.no>
2022-09-18 00:40:20 +0200

    color2sepia numba implementation

--- assignment3/instapy/numba_filters.py
+++ assignment3/instapy/numba_filters.py
@@ -22,6 +22,7 @@ def numba_color2gray(image: np.array) -> np.array:
     return gray_image
 
 
+@jit(nopython=True)
 def numba_color2sepia(image: np.array) -> np.array:
     """Convert rgb pixel array to sepia
 
@@ -31,10 +32,20 @@ def numba_color2sepia(image: np.array) -> np.array:
         np.array: sepia_image
     """
     sepia_image = np.empty_like(image)
-    # Iterate through the pixels
-    # applying the sepia matrix
 
-    ...
+    sepia_matrix = [
+        [ 0.393, 0.769, 0.189],
+        [ 0.349, 0.686, 0.168],
+        [ 0.272, 0.534, 0.131],
+    ]
+    
+    for row in range(image.shape[0]):  # height
+        for col in range(image.shape[1]):  # width
+            red, green, blue = image[row][col]
+            avg_weighted_red = min(255, (red * sepia_matrix[0][0] + green * sepia_matrix[0][1] + blue * sepia_matrix[0][2]) / 3)
+            avg_weighted_green = min(255, (red * sepia_matrix[1][0] + green * sepia_matrix[1][1] + blue * sepia_matrix[1][2]) / 3)
+            avg_weighted_blue = min(255, (red * sepia_matrix[2][0] + green * sepia_matrix[2][1] + blue * sepia_matrix[2][2]) / 3)
+            sepia_image[row][col] = [avg_weighted_red, avg_weighted_green, avg_weighted_blue]
 
     # Return image
     # don't forget to make sure it's the right type!
@@ -51,3 +62,10 @@ if __name__ == '__main__':
     image2 = io.read_image(filename='assignment3/test_image2.jpg')
     gray_image2 = numba_color2gray(image2)
     io.display(gray_image2)
+
+    sepia_image = numba_color2sepia(image)
+    io.display(sepia_image)
+
+    # Test 2 images because of numba's inherent cold start compilation
+    sepia_image2 = numba_color2sepia(image2)
+    io.display(sepia_image2)
