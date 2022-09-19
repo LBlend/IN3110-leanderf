2be3c7f6360659d72a283e1306be70982127e481
leanderf <leanderf@ifi.uio.no>
2022-09-19 19:11:46 +0200

    python color2gray test

--- assignment3/test/test_python.py
+++ assignment3/test/test_python.py
@@ -3,12 +3,17 @@ from instapy.python_filters import python_color2gray, python_color2sepia
 
 def test_color2gray(image):
     # run color2gray
-    ...
+    grayscale = python_color2gray(image)
+    
     # check that the result has the right shape, type
-    ...
-    # assert uniform r,g,b values
-    ...
+    assert grayscale.shape == image.shape
+    assert grayscale.dtype == image.dtype
 
+    # assert uniform r,g,b values
+    # checking top left 5x5 pixels
+    for i in range(5):
+        for j in range(5):
+            assert grayscale[i][j][0] == grayscale[i][j][1] == grayscale[i][j][2]
 
 def test_color2sepia(image):
     # run color2sepia
