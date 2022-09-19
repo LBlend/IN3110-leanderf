507d819df43a7869205d53a65d96d70de053d0de
leanderf <leanderf@ifi.uio.no>
2022-09-19 20:52:11 +0200

    initial numba tests

--- assignment3/test/test_numba.py
+++ assignment3/test/test_numba.py
@@ -4,8 +4,12 @@ import numpy.testing as nt
 
 
 def test_color2gray(image, reference_gray):
-    ...
+    # run color2gray
+    grayscale = numba_color2gray(image)
+    assert grayscale == reference_gray
 
 
 def test_color2sepia(image, reference_sepia):
-    ...
+    # run color2sepia
+    sepia = numba_color2sepia(image)
+    assert image == reference_sepia
