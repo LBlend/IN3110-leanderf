5b35fb0fd56debd4535ebaf8a1c2fdf9f78906e3
leanderf <leanderf@ifi.uio.no>
2022-11-03 23:18:03 +0100

    complete task 4 (bonus)

--- assignment4/collect_dates.py
+++ assignment4/collect_dates.py
@@ -19,7 +19,7 @@ month_names = [
     "December",
 ]
 
-...
+month_abbreviations = list(map(lambda month: month[:3], month_names))
 
 
 def get_date_patterns() -> Tuple[str, str, str]:
@@ -32,11 +32,13 @@ def get_date_patterns() -> Tuple[str, str, str]:
 
     # Regex to capture days, months and years with numbers
     # year should accept a 4-digit number between at least 1000-2029
-    year = r"(?P<year>...)"
+    year = r"(?P<year>\d{4})"
+
     # month should accept month names or month numbers
-    month = r"(?P<month>...)"
+    month = rf"(?<!\d)(?P<month>1[0-2]?|0[1-9]|{'|'.join(month_names)}|{'|'.join(month_abbreviations)})(?!\d)"
+
     # day should be a number, which may or may not be zero-padded
-    day = r"(?P<day>...)"
+    day = r"(?<!\d)(?P<day>[0-2]{0,1}\d{1}|3[0-1]{1})(?!\d)"
 
     return year, month, day
 
@@ -54,10 +56,18 @@ def convert_month(s: str) -> str:
     """
     # If already digit do nothing
     if s.isdigit():
-        ...
+        return zero_pad(s)
 
-    # Convert to number as string
-    ...
+    # Not a digit. Make sure first letter is capitalized
+    s = s.title()
+
+    # Get month number
+    if len(s) == 3:
+        s = str(month_abbreviations.index(s) + 1)
+    else:
+        s = str(month_names.index(s) + 1)
+
+    return zero_pad(s)
 
 
 def zero_pad(n: str):
@@ -68,7 +78,10 @@ def zero_pad(n: str):
     You don't need to use this function,
     but you may find it useful.
     """
-    ...
+    if len(n) == 1:
+        return f"0{n}"
+
+    return n
 
 
 def find_dates(text: str, output: str = None) -> list:
@@ -81,26 +94,52 @@ def find_dates(text: str, output: str = None) -> list:
     """
     year, month, day = get_date_patterns()
 
+    # Pattern that matches spaces between date segments
+    space_pattern = r"[ \/,\-]{1,2}"
+
     # Date on format YYYY/MM/DD - ISO
-    ISO = ...
+    ISO = rf"{year}{space_pattern}{month}{space_pattern}{day}"
 
     # Date on format DD/MM/YYYY
-    DMY = ...
+    DMY = rf"{day}{space_pattern}{month}{space_pattern}{year}"
 
     # Date on format MM/DD/YYYY
-    MDY = ...
+    MDY = rf"{month}{space_pattern}{day}{space_pattern}{year}"
 
     # Date on format YYYY/MM/DD
-    YMD = ...
+    # YMD = ISO
+    # YMD is redundant since ISO matches it, hence I've commented it out
 
     # list with all supported formats
-    formats = ...
+    formats = [DMY, MDY, ISO]
     dates = []
 
     # find all dates in any format in text
-    ...
+    for pattern in formats:
+        found_dates = re.findall(pattern, text, flags=re.IGNORECASE)
+
+        if not found_dates:
+            continue
+
+        for date in found_dates:
+            # Convert list of match groups to string
+            date = " ".join(date)
+
+            # Reorder date to Year Month Day
+            date = re.sub(pattern, r"\g<year>/\g<month>/\g<day>", date, flags=re.IGNORECASE)
+
+            # Convert month to number
+            date = re.sub(month, lambda m: convert_month(m.group("month")), date, flags=re.IGNORECASE)
+
+            # Zero-pad day
+            date = re.sub(day, lambda d: zero_pad(d.group("day")), date, flags=re.IGNORECASE)
+
+            # Add to list
+            dates.append(date)
+
     # Write to file if wanted
     if output:
-        ...
+        with open(output, "w") as f:
+            f.write("\n".join(dates))
 
     return dates
